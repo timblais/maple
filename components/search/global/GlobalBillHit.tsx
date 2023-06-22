f7cd6501c006d62099184e9d204991557a4f762d
Tim Blais <tblais189@gmail.com>
2023-06-21 23:04:33 -0400

    initial build of bill hits for global search unstyled

---
+++ components/search/global/GlobalBillHit.tsx
@@ -0,0 +1,44 @@
+import { Highlight } from "@alexjball/react-instantsearch-hooks-web"
+import { maple } from "components/links"
+import { Hit } from "instantsearch.js"
+import Link from "next/link"
+import { Card, Col } from "../../bootstrap"
+import { formatBillId } from "../../formatting"
+import { ListGroup } from "react-bootstrap" // Need to move this into bootstrap components file?
+
+type BillRecord = {
+  number: string
+  title: string
+  city?: string
+  court: number
+  currentCommittee?: string
+  testimonyCount: number
+  endorseCount: number
+  opposeCount: number
+  neutralCount: number
+  nextHearingAt?: number
+  latestTestimonyAt?: number
+  cosponsors: string[]
+  cosponsorCount: number
+  primarySponsor?: string
+}
+
+export const GlobalBillHit = ({ hit }: { hit: Hit<BillRecord> }) => {
+  const url = maple.bill({ id: hit.number, court: hit.court })
+  return (
+    <Link href={url}>
+      <a style={{ all: "unset" }} className="w-100">
+        <ListGroup.Item>
+          <img src="./BillSearchResult.svg" alt="Bill Icon" />
+          <div>
+            <h6>
+              {formatBillId(hit.number)} -{" "}
+              <Highlight attribute="title" hit={hit} />
+            </h6>
+            <span>Bill</span>
+          </div>
+        </ListGroup.Item>
+      </a>
+    </Link>
+  )
+}
